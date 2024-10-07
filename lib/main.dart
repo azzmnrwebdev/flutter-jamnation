2f4365775d16f11ad1b51abae553c6373d1d7476
azzmnrwebdev <azzmnrwebdev@gmail.com>
2024-10-07 13:35:57 +0700

    menambahkan splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:jamnation_mobile/router/route.dart';
+import 'package:jamnation_mobile/screens/home_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:jamnation_mobile/screens/auth/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +12,43 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
+    return const MaterialApp(
+      home: SplashScreen(),
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  Future<String?> _getToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<String?>(
+        future: _getToken(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (snapshot.hasData && snapshot.data != null) {
+              return const HomeScreen();
+            } else {
+              return const LoginScreen();
+            }
+          }
+        },
+      ),
     );
   }
 }
--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,20 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
       ),
-      body: const Center(),
     );
+
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: Container(
+      color: const Color(0xFF0C51A0),
+      height: size.height,
+      width: size.width,
+      child: Stack(
+        children: [
+          SizedBox(
+            height: size.height * 0.5,
+            width: size.width,
+            child: Image.asset(
+              'assets/images/background.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            height: size.height * 0.65,
+            top: size.height * 0.35,
+            left: 0,
+            right: 0,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Color(0xFF0C51A0),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+              ),
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                children: [
+                  const SizedBox(height: 36),
+                  Center(
+                    child: Image.asset(
+                      'assets/images/logo_putih.png',
+                      width: 200,
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Masuk ke akun Anda',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
